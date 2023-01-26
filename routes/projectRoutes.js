3a0e6f816d42ffdf813f7eaf6e574c9eac2b5024
pooja1902 <poojakaur191998@gmail.com>
2023-01-26 20:05:56 +1100

    updated

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -1,48 +1,51 @@
-var express = require("express");
+var express = require("express")
 var router = express.Router();
 let client = require("../dbConnect");
-let projectCollection;
-let Controller = require("../controller");
+let projectsCollection;
+let controller = require("../controller");
 
-//setTimeout(() => {
-//    projectCollection = client.mongoClient.db().collection("Projects");    
-//}, 2000)
+// setTimeout(() => {
+//     projectsCollection = client.mongoClient.db().collection("projects");
+// }, 2000)
 
-//insert project
-//const insertProjects = (project,callback) => {
-//        projectCollection.insert(project,callback);
-//    }
+// const insertProjects = (project,callback) => { 
+//     projectCollection.insert(project,callback); 
+// } 
 
-    //post api
-    router.post('/',(req,res) => {
-        controller.projectController.createProjects(req, res)
- //           console.log("New Project added", req.body)
- //           var newProject = req.body;
- //           insertProjects(newProject,(err,result) => {
- //               if(err) {
- //                   res.json({statusCode: 400, message: err})
- //               }
- //               else {
- //                   res.json({statusCode: 200, message:"Project Successfully added", data: result})
- //               }
- //           })
-        }) 
+//post project ..
+ router.post('/',(req,res) => { 
+    controller.projectController.createProjects(req,res)
+ })
 
-        // get project...
-//const getProjects = (callback) => {
-//        projectCollection.find({}).toArray(callback);
-//    }
+//     console.log("New Project added", req.body) 
+//     var newProject = req.body; 
+//     insertProjects(newProject,(err,result) => {
+//         if(err) { 
+//             res.json({statusCode: 400, message: err}) 
+//         } 
+//         else { 
+//             res.json({statusCode: 200, message:"Project Successfully added", data: result}) 
+//         } 
+//     }) 
+// }) 
 
-    router.get('/',(req,res) => {
-      controller.projectController.retrieveProjects(req, res)
-  //      getProjects((err,result) => {
- //           if(err) {
- //               res.json({statusCode: 400, message: err})
- //           }
- //           else {
- //               res.json({statusCode: 200, message:"Success", data: result})
- //           }
- //       })
-    }) 
 
-    module.exports = router;
+
+// // get project... 
+// const getProjects = (callback) => { 
+//     projectCollection.find({}).toArray(callback); 
+// } 
+
+router.get('/',(req,res) => { 
+    controller.projectController.retrieveProjects(req,res)
+    // getProjects((err,result) => { 
+    //     if(err) { 
+    //         res.json({statusCode: 400, message: err}) 
+    //     } 
+    //     else { 
+    //         res.json({statusCode: 200, message:"Success", data: result}) 
+    //     } 
+    // }) 
+}) 
+
+module.exports = router;
